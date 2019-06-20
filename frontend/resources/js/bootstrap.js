9fbfb370c6def8fb3945f3f8e7f9ccd6d67e041e
Douglas <asrdfsdvs@gmail.com>
2019-06-20 19:56:31 -0300

    Correção do frontend

--- frontend/resources/js/bootstrap.js
+++ frontend/resources/js/bootstrap.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 window.axios = axios.create({
     baseURL: '//pweb.local:' + require('./.env.json').port,
 });
