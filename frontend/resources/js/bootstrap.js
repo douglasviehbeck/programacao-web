b3944ab5153caf287eea9fc7eaa55b9b16c5071e
Douglas <asrdfsdvs@gmail.com>
2019-06-20 21:47:48 -0300

    Correção do host vue

--- frontend/resources/js/bootstrap.js
+++ frontend/resources/js/bootstrap.js
@@ -3,5 +3,5 @@ const axios = require('axios');
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 window.axios = axios.create({
-    baseURL: '//pweb.local:' + require('./.env.json').port,
+    baseURL: '//' + window.location.hostname + ':' + require('./.env.json').port,
 });
