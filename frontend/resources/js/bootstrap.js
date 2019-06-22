80c79c323e602b6a815db06cff653fbd0791c8ed
Douglas <asrdfsdvs@gmail.com>
2019-06-22 14:02:28 -0300

    console da url da api

--- frontend/resources/js/bootstrap.js
+++ frontend/resources/js/bootstrap.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
+console.log('//' + window.location.hostname + ':' + require('./.env.json').port);
+
 window.axios = axios.create({
     baseURL: '//' + window.location.hostname + ':' + require('./.env.json').port,
 });
